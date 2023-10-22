ae9159a9bd06d41b3a89a48f16c4c62c9570e135
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 15:55:06 +0600

    initial commit

---
+++ app/src/main/java/com/saadahmedev/hpcapi/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.saadahmedev.hpcapi.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saadahmedev.hpcapi.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
