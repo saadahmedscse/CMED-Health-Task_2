34034d8614921c2b1436ecfb40be53ada6cd8af5
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 17:34:45 +0600

    fixed wand length data type from int to double

--- app/src/main/java/com/saadahmedev/hpcapi/data/dto/Wand.kt
+++ app/src/main/java/com/saadahmedev/hpcapi/data/dto/Wand.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class Wand(
     @SerializedName("wood") var wood: String? = null,
     @SerializedName("core") var core: String? = null,
-    @SerializedName("length") var length: Int? = null
+    @SerializedName("length") var length: Double? = null
 )
