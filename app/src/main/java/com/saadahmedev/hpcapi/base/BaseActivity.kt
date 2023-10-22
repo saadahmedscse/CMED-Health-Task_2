0f30ad6946928d5f0494db8274d9e2570d0b773c
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 16:12:44 +0600

    created BaseFragment

---
+++ app/src/main/java/com/saadahmedev/hpcapi/base/BaseActivity.kt
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2018-2023 Saad Ahmed
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.saadahmedev.hpcapi.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import com.google.android.material.snackbar.Snackbar
+import com.saadahmedev.hpcapi.databinding.AppToolbarBinding
+import com.saadahmedev.hpcapi.helper.SnackbarActionListener
+import com.saadahmedev.hpcapi.helper.observe
+import com.saadahmedev.hpcapi.helper.onClicked
+import com.saadahmedev.hpcapi.helper.snackBar
+import com.saadahmedev.hpcapi.helper.toast
+import com.saadahmedev.hpcapi.viewmodel.ToolbarViewModel
+
+abstract class BaseActivity<BINDING: ViewBinding>(private val bindingInflater: (inflater: LayoutInflater) -> BINDING) : AppCompatActivity() {
+
+    private lateinit var _binding: BINDING
+    private val baseViewModel by viewModels<ToolbarViewModel>()
+
+    val binding: BINDING
+        get() = _binding
+
+    abstract val toolbarBinding: AppToolbarBinding?
+
+    abstract fun onActivityCreate(savedInstanceState: Bundle?)
+
+    abstract fun observeData()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingInflater.invoke(layoutInflater)
+        setContentView(binding.root)
+        initToolbar()
+        onActivityCreate(savedInstanceState)
+        observeData()
+    }
+
+    private fun initToolbar() {
+        if (toolbarBinding != null) {
+            observe(baseViewModel.title) {
+                toolbarBinding?.toolbarTitle?.text = it
+            }
+
+            observe(baseViewModel.isBackButtonVisible) {
+                toolbarBinding?.toolbarBtn?.visibility = if (it) View.VISIBLE else View.GONE
+            }
+        }
+
+        toolbarBinding?.toolbarBtn?.onClicked {
+            super.onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    fun String?.shortSnackBar(action: String, listener: SnackbarActionListener) {
+        this?.let { showSnackBar(it, action, Snackbar.LENGTH_SHORT, listener) }
+    }
+
+    fun String?.longSnackBar(action: String, listener: SnackbarActionListener) {
+        this?.let { showSnackBar(it, action, Snackbar.LENGTH_LONG, listener) }
+    }
+
+    fun String?.shortSnackBar() {
+        this?.let {
+            showSnackBar(it, "Close", Snackbar.LENGTH_SHORT, object : SnackbarActionListener {
+                override fun onActionClicked(snackbar: Snackbar) {
+                    snackbar.dismiss()
+                }
+            })
+        }
+    }
+
+    fun String?.longSnackBar() {
+        this?.let {
+            showSnackBar(it, "Close", Snackbar.LENGTH_LONG, object : SnackbarActionListener {
+                override fun onActionClicked(snackbar: Snackbar) {
+                    snackbar.dismiss()
+                }
+            })
+        }
+    }
+
+    fun String?.shortToast() {
+        this?.let { showToast(it, Toast.LENGTH_SHORT) }
+    }
+
+    fun String?.longToast() {
+        this?.let { showToast(it, Toast.LENGTH_LONG) }
+    }
+
+    private fun showSnackBar(message: String, action: String, duration: Int, listener: SnackbarActionListener) {
+        snackBar(_binding.root, message, action, duration, listener)
+    }
+
+    private fun showToast(message: String, duration: Int) {
+        toast(this, message, duration)
+    }
+}
