890ae17339acdac30ba24cb5525f65c132150a88
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 16:50:06 +0600

    created HpCharacter Model

---
+++ app/src/main/java/com/saadahmedev/hpcapi/domain/model/HpCharacter.kt
@@ -0,0 +1,9 @@
+package com.saadahmedev.hpcapi.domain.model
+
+data class HpCharacter(
+    val id: String? = null,
+    val picture: String? = null,
+    val name: String? = null,
+    val actorName: String? = null,
+    val houseName: String? = null
+)
