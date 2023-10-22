636eddbecb10986a1ea4755bc8a25dc59febe8e9
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 18:22:55 +0600

    created bottom sheet progress dialog

---
+++ app/src/main/java/com/saadahmedev/hpcapi/util/ProgressDialog.java
@@ -0,0 +1,33 @@
+package com.saadahmedev.hpcapi.util;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.saadahmedev.hpcapi.R;
+
+public class ProgressDialog {
+
+    private final BottomSheetDialog bottomSheetDialog;
+
+    private ProgressDialog(Context context) {
+        bottomSheetDialog = new BottomSheetDialog(context, R.style.BottomSheetStyle);
+    }
+
+    public static ProgressDialog getInstance(Context context) {
+        return new ProgressDialog(context);
+    }
+
+    public void show(String message) {
+        bottomSheetDialog.setContentView(R.layout.item_layout_bottom_sheet);
+        TextView progressText = bottomSheetDialog.findViewById(R.id.tv_progress_message);
+        assert progressText != null;
+        progressText.setText(message);
+        bottomSheetDialog.setCancelable(false);
+        bottomSheetDialog.show();
+    }
+
+    public void dismiss() {
+        if (bottomSheetDialog.isShowing()) bottomSheetDialog.dismiss();
+    }
+}
