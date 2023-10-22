7d7c08a456209049a9d624a203074cf7ab41297e
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 23:48:03 +0600

    added function to HPCharacterEntity

--- app/src/main/java/com/saadahmedev/hpcapi/domain/model/HpCharacter.kt
+++ app/src/main/java/com/saadahmedev/hpcapi/domain/model/HpCharacter.kt
@@ -1,9 +1,17 @@
 package com.saadahmedev.hpcapi.domain.model
 
+import com.saadahmedev.hpcapi.caching.entity.HpCharacterEntity
+
 data class HpCharacter(
     val id: String? = null,
     val picture: String? = null,
     val name: String? = null,
     val actorName: String? = null,
     val houseName: String? = null
-)
+) {
+    fun toHpCharacterEntity(): HpCharacterEntity {
+        return HpCharacterEntity(
+            id!!, picture, name, actorName, houseName
+        )
+    }
+}
