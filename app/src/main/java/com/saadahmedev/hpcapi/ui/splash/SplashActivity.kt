34e0d5258cd60cb27b196ab8c6771c067bd1ea59
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 15:54:30 +0600

    changed splash activity package

--- app/src/main/java/com/saadahmedev/hpcapi/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/saadahmedev/hpcapi/ui/splash/SplashActivity.kt
@@ -1,10 +1,11 @@
- package com.saadahmedev.hpcapi
+ package com.saadahmedev.hpcapi.ui.splash
 
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.saadahmedev.hpcapi.R
 
-@SuppressLint("CustomSplashScreen")
+ @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
