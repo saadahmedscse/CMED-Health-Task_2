30b5197577f93287bc896da943f8e1431667b3c3
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 15:58:56 +0600

    navigate to dashboard activity after 1500ms

--- app/src/main/java/com/saadahmedev/hpcapi/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/saadahmedev/hpcapi/ui/splash/SplashActivity.kt
@@ -4,11 +4,22 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.saadahmedev.hpcapi.R
+import com.saadahmedev.hpcapi.ui.dashboard.DashboardActivity
+import com.saadahmedev.hpcapi.ui.helper.delay
+import com.saadahmedsoft.shortintent.Anim
+import com.saadahmedsoft.shortintent.ShortIntent
 
  @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        delay(1500) {
+            ShortIntent.getInstance(this)
+                .addDestination(DashboardActivity::class.java)
+                .addTransition(Anim.FADE)
+                .finish(this)
+        }
     }
 }
