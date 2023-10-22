241e10e6746b0e8951ae8208e028799450761501
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 18:46:56 +0600

    observe character details from view model

--- app/src/main/java/com/saadahmedev/hpcapi/ui/dashboard/tabs/characterDetails/CharacterDetailsFragment.kt
+++ app/src/main/java/com/saadahmedev/hpcapi/ui/dashboard/tabs/characterDetails/CharacterDetailsFragment.kt
@@ -1,8 +1,13 @@
 package com.saadahmedev.hpcapi.ui.dashboard.tabs.characterDetails
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
 import com.saadahmedev.hpcapi.base.BaseFragment
 import com.saadahmedev.hpcapi.databinding.FragmentCharacterDetailsBinding
+import com.saadahmedev.hpcapi.helper.observe
+import com.saadahmedev.hpcapi.ui.dashboard.tabs.characterDetails.viewmodel.HpCharacterDetailsViewModel
+import com.saadahmedev.hpcapi.util.ProgressDialog
+import com.saadahmedev.hpcapi.util.ResponseState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,7 +18,33 @@ class CharacterDetailsFragment : BaseFragment<FragmentCharacterDetailsBinding>(F
     override val isBackButtonVisible: Boolean
         get() = true
 
-    override fun onFragmentCreate(savedInstanceState: Bundle?) {}
+    private val viewModel by viewModels<HpCharacterDetailsViewModel>()
+    private lateinit var progressDialog: ProgressDialog
 
-    override fun observeData() {}
+    override fun onFragmentCreate(savedInstanceState: Bundle?) {
+        progressDialog = ProgressDialog.getInstance(requireContext())
+        viewModel.getHpCharacterDetails(tinyDB.getString("character_id", null))
+    }
+
+    override fun observeData() {
+        observe(viewModel.hpCharacterDetails) {
+            when (it) {
+                is ResponseState.Loading -> {
+                    progressDialog.show("Loading details for ${tinyDB.getString("character_name", null)}.")
+                }
+
+                is ResponseState.Success -> {
+                    it.data?.let { character ->
+                        character.gender.shortSnackBar()
+                    }
+                    progressDialog.dismiss()
+                }
+
+                is ResponseState.Error -> {
+                    it.message.longSnackBar()
+                    progressDialog.dismiss()
+                }
+            }
+        }
+    }
 }
