dc074b7b0f63efd1142e53ecc503af1b873038bc
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 17:34:32 +0600

    fetch hp characters from view model

--- app/src/main/java/com/saadahmedev/hpcapi/ui/dashboard/tabs/home/HomeFragment.kt
+++ app/src/main/java/com/saadahmedev/hpcapi/ui/dashboard/tabs/home/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.saadahmedev.hpcapi.ui.dashboard.tabs.home
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
 import com.saadahmedev.hpcapi.R
 import com.saadahmedev.hpcapi.base.BaseFragment
 import com.saadahmedev.hpcapi.databinding.FragmentHomeBinding
+import com.saadahmedev.hpcapi.helper.observe
+import com.saadahmedev.hpcapi.ui.dashboard.tabs.home.viewmodel.HpCharacterListViewModel
 import com.saadahmedev.hpcapi.util.Constants.AppInfo.APP_NAME
+import com.saadahmedev.hpcapi.util.ResponseState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,7 +19,27 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     override val isBackButtonVisible: Boolean
         get() = false
 
-    override fun onFragmentCreate(savedInstanceState: Bundle?) {}
+    private val viewModel by viewModels<HpCharacterListViewModel>()
 
-    override fun observeData() {}
+    override fun onFragmentCreate(savedInstanceState: Bundle?) {
+        viewModel.getCharacters()
+    }
+
+    override fun observeData() {
+        observe(viewModel.hpCharacterList) {
+            when (it) {
+                is ResponseState.Loading -> {
+                    //
+                }
+
+                is ResponseState.Success -> {
+                    it.data!![0].name.longSnackBar()
+                }
+
+                is ResponseState.Error -> {
+                    it.message.longSnackBar()
+                }
+            }
+        }
+    }
 }
