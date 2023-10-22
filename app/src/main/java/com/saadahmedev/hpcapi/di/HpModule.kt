32eb1d27411391e378416fcde019a6a99338553b
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 17:01:47 +0600

    provide singleton hp api

---
+++ app/src/main/java/com/saadahmedev/hpcapi/di/HpModule.kt
@@ -0,0 +1,26 @@
+package com.saadahmedev.hpcapi.di
+
+import com.saadahmedev.hpcapi.data.source.HpApi
+import com.saadahmedev.hpcapi.util.Constants.Api.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HpModule {
+
+    @Provides
+    @Singleton
+    fun provideHpApi(): HpApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HpApi::class.java)
+    }
+}
