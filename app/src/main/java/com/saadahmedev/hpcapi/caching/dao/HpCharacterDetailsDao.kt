6b3e8d50da3bd1e1aff02ea4f077cdf4b42ecc38
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 23:51:05 +0600

    created HpCharacterDetailsDao

---
+++ app/src/main/java/com/saadahmedev/hpcapi/caching/dao/HpCharacterDetailsDao.kt
@@ -0,0 +1,18 @@
+package com.saadahmedev.hpcapi.caching.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.saadahmedev.hpcapi.caching.entity.HpCharacterDetailsEntity
+
+@Dao
+interface HpCharacterDetailsDao {
+
+    @Query("SELECT * FROM HpCharacterDetails WHERE id=:id")
+    fun getCharacterDetails(id: String): LiveData<HpCharacterDetailsEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertCharacterDetails(characterDetails: HpCharacterDetailsEntity)
+}
