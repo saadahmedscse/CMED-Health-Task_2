f96b4fa2d9f970927ecc00a35c142553583536cb
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 16:59:24 +0600

    added getCharacters and getCharactersById endpoints

--- app/src/main/java/com/saadahmedev/hpcapi/data/source/HpApi.kt
+++ app/src/main/java/com/saadahmedev/hpcapi/data/source/HpApi.kt
@@ -1,4 +1,17 @@
 package com.saadahmedev.hpcapi.data.source
 
+import com.saadahmedev.hpcapi.data.dto.HpCharacterResponse
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
 interface HpApi {
+
+    @Headers("Accept: application/json")
+    @GET("characters")
+    suspend fun getCharacters(): List<HpCharacterResponse>
+
+    @Headers("Accept: application/json")
+    @GET("character/{id}")
+    suspend fun getCharacter(@Path("id") id: String): List<HpCharacterResponse>
 }
