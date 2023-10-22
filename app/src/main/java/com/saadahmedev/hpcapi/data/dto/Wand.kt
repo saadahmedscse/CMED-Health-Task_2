adbf5f4e37ecd27c9d422a04590cfcd182e0e144
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 16:49:49 +0600

    Hp Api Character Response

---
+++ app/src/main/java/com/saadahmedev/hpcapi/data/dto/Wand.kt
@@ -0,0 +1,9 @@
+package com.saadahmedev.hpcapi.data.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class Wand(
+    @SerializedName("wood") var wood: String? = null,
+    @SerializedName("core") var core: String? = null,
+    @SerializedName("length") var length: Int? = null
+)
