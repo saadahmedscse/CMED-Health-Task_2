79f692035a15c1b11741f69de30b305a02f3dc32
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 15:53:18 +0600

    changed name main activity to splash activity

--- app/src/main/java/com/saadahmedev/hpcapi/SplashActivity.kt
+++ app/src/main/java/com/saadahmedev/hpcapi/SplashActivity.kt
@@ -1,11 +1,13 @@
  package com.saadahmedev.hpcapi
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
