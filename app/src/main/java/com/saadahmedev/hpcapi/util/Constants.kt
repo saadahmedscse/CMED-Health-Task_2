b7442931fe1b7ac973e3f72e51a8d6f291c8b5fb
saadahmedscse <saadahmedscse@gmail.com>
2023-10-22 16:14:44 +0600

    created Constants class

---
+++ app/src/main/java/com/saadahmedev/hpcapi/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.saadahmedev.hpcapi.util
+
+class Constants {
+    object AppInfo {
+        const val APP_NAME = "Harry Potter Characters"
+    }
+
+    object Api {
+        const val BASE_URL = "https://hp-api.onrender.com/api/"
+    }
+}
